37dd2cdc64fae54f02e58b4bed0756c20d187c31
Hossam Cj <hossamcj@gmail.com>
2023-09-27 01:59:42 +0300

    Add the last touch xD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,36 @@
 import { TodoItem } from "@/components/TodoItem";
 import { prisma } from "@/db";
 import Link from "next/link";
-import { Fragment } from "react";
 
 function getTodos() {
     return prisma.todo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+    "use server";
+
+    await prisma.todo.update({ where: { id }, data: { complete } });
+}
+
 export default async function Home() {
     const todos = await getTodos();
 
-    // await prisma.todo.create({ data: { title: "Todo test", complete: false } });
-
     return (
-        <Fragment>
+        <>
             <header className="flex justify-between items-center mb-4">
-                <h1 className="text-4xl">قائمة المهام</h1>
+                <h1 className="text-2xl">Todos</h1>
                 <Link
+                    className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
                     href="/new"
-                    className="
-                      text-3xl border border-slate-300 text-slate-300 px-2 py-1 
-                      rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none
-                    "
                 >
-                    أضف مهمة
+                    New
                 </Link>
             </header>
             <ul className="pl-4">
                 {todos.map((todo) => (
-                    <TodoItem key={todo.id} {...todo} />
+                    <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
                 ))}
             </ul>
-        </Fragment>
+        </>
     );
 }
